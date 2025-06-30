d5417ac6c21e305ac839648de75e8429ecfd8bfa
mnveee <vishmag09@gmail.com>
2025-06-30 21:00:55 +0530

    Update Mongoose connection with increased timeout

--- config/db.js
+++ config/db.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
 
 function connectToDB() {
-  mongoose.connect(process.env.MONGO_URI).then(() => {
-    console.log("Connected to MongoDB successfully");
-  });
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000, // Increase timeout to 30s
+    })
+    .then(() => {
+      console.log("Connected to MongoDB successfully");
+    });
 }
 
 module.exports = connectToDB;
